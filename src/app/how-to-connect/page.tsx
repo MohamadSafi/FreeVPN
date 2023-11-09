5be50ea1a3c68352b9f20ffb4aa73f9c062480bc
MohamadSafi <the.mohammad.safi@gmail.com>
2023-11-09 16:39:37 +0300

    Responsive "How to connect"

--- src/app/how-to-connect/page.tsx
+++ src/app/how-to-connect/page.tsx
@@ -6,7 +6,7 @@ import PageNavbar from "@/components/custom/PageNavbar";
 export default function HowToConnect() {
   return (
     <main>
-      <div className="lg:mx-48 xl:mx-56 2xl:mx-56 3xl:mx-56 mb-24">
+      <div className="mx-2 lg:mx-48 xl:mx-56 2xl:mx-56 3xl:mx-56 mb-24">
         <PageNavbar />
         <InstructionHero />
         <InstructionSteps />
