ec2a006a8a098edd3b9df8cb207b7a1920fcc87f
MohamadSafi <the.mohammad.safi@gmail.com>
2023-11-26 10:24:53 +0300

    Adding canonical to Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Free Me VPN",
   description: "Free Me VPN is a Free vpn App",
+  alternates: {
+    canonical: "https://freemevpn.com/",
+  },
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
