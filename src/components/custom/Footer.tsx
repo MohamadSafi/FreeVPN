2885a55848bc268ea3a92e96678fd1a4c32edb54
MohamadSafi <the.mohammad.safi@gmail.com>
2023-11-30 10:22:26 +0300

    Updating footer for better SEO

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -7,9 +7,11 @@ export default function Footer() {
       <div className="flex justify-center flex-col items-center">
         <p className="text-xl ">Free me VPN</p>
         <p className="text-xs mb-16">
-          Free me VPN is a highly secure vpn service that will always be free.{" "}
+          Discover the ultimate freedom with Free Me VPN – your forever-free,
+          top-tier secure VPN service, ensuring free online safety at no cost
+          with Free Me VPN.{" "}
         </p>
-        <p className="text-xs opacity-60 ">©2023 - FreeVPN</p>
+        <p className="text-xs opacity-60 ">©2023 - FreeMeVPN</p>
       </div>
     </main>
   );
